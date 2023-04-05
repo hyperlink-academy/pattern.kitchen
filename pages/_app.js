46dc0e3272fe8de5f491a275792e93726959ab5e
Brendan Schlagel <brendan.schlagel@gmail.com>
2023-04-05 17:14:24 -0400

    remove boilerplate

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
+
+const Layout = (props) => {
+  return (
+    <div style={{ maxWidth: "48rem", margin: "auto", padding: "1rem" }}>
+      {props.children}
+    </div>
+  );
+};
